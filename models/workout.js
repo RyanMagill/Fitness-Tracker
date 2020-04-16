909ab36376b45328d1dcb54870c2617c478a1663
Ryan <ryanmagill97@gmail.com>
2020-04-16 13:03:40 -0500

    reorganized file structure and made Schema

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let workoutSchema = new Schema(
+    {
+        day: {type:Date, default: ()=> new Date()},
+        exercises: [{
+            type: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: String
+            },
+            distance: {
+                type: String
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            
+        }]
+    }
+);
+
+const ExerciseModel = mongoose.model("Exercise", workoutSchema);
+
+module.exports = ExerciseModel;
